695276f63f5f6d02fb6a74c39bf0cfdab91a9402
edgard <edgard.habanbou@hotmail.com>
2024-02-02 20:21:09 +0200

    Login Page

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\PullRequestController;
 
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('login');
 });
 
 Route::get('/pull-requests/{ownerName}/{repoName}', [PullRequestController::class, 'Main']);
