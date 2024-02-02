ccedb843c1674dcf87702aada5c76d0cdbdd72ce
edgard <edgard.habanbou@hotmail.com>
2024-02-02 17:08:47 +0200

    Removed old functions and added fetchPullRequests to make it more dynamic

--- app/Http/Controllers/PullRequestController.php
+++ app/Http/Controllers/PullRequestController.php
@@ -6,39 +6,26 @@ use GuzzleHttp\Client;
 
 class PullRequestController extends Controller
 {
-    public function fetchOpenPullRequests()
+    public function fetchPullRequests($parameter)
     {
-        try {
-            $client = new Client();
-            $response = $client->request('GET', 'https://api.github.com/repos/woocommerce/woocommerce/pulls', [
-                'headers' => [
-                    'Accept' => 'application/vnd.github+json',
-                    'Authorization' => 'Bearer ' . env('GITHUB_ACCESS_TOKEN'),
-                    'X-GitHub-Api-Version' => '2022-11-28'
-                ]
-            ]);
-            $pullRequests = json_decode($response->getBody()->getContents(), true);
-            return $pullRequests;
-        } catch (\Exception $e) {
-            return response()->json(['error' => $e->getMessage()], 500);
-        }
+        $client = new Client();
+        $response = $client->request('GET', 'https://api.github.com/search/issues?q=is:pr+is:open+repo:woocommerce/woocommerce' . $parameter, [
+            'headers' => [
+                'Accept' => 'application/vnd.github+json',
+                'Authorization' => 'Bearer ' . env('GITHUB_ACCESS_TOKEN'),
+                'X-GitHub-Api-Version' => '2022-11-28'
+            ]
+        ]);
+        $pullRequests = json_decode($response->getBody()->getContents(), true);
+        return $pullRequests["items"];
     }
 
-    public function fetchOldOpenPullRequests($pullRequests)
-    {
-        $oldPullRequests = [];
-        foreach ($pullRequests as $pullRequest) {
-            if (strtotime($pullRequest['created_at']) < strtotime('-14 days')) {
-                $oldPullRequests[] = $pullRequest;
-            }
-        }
-        return $oldPullRequests;
-    }
+
 
     public function Main()
     {
-        $pullRequests = $this->fetchOpenPullRequests();
-        $oldPullRequests = $this->fetchOldOpenPullRequests($pullRequests);
+        $twoWeeksAgo = date('Y-m-d', strtotime('-2 weeks'));
+        $oldPullRequests = $this->fetchPullRequests("+created:<" . $twoWeeksAgo);
         return response()->json($oldPullRequests);
     }
 }
