a5a019f5b26aeace78133bdd6762ac29e4662188
edgard <edgard.habanbou@hotmail.com>
2024-02-02 15:21:28 +0200

    Fixed PullRequestController functionality

--- app/Http/Controllers/PullRequestController.php
+++ app/Http/Controllers/PullRequestController.php
@@ -10,14 +10,15 @@ class PullRequestController extends Controller
     {
         try {
             $client = new Client();
-            $response = $client->request('GET', 'https://api.github.com/repos/OWNER/REPO/pulls', [
+            $response = $client->request('GET', 'https://api.github.com/repos/woocommerce/woocommerce/pulls', [
                 'headers' => [
                     'Accept' => 'application/vnd.github+json',
-                    'Authorization' => 'Bearer ' + env('GITHUB_ACCESS_TOKEN'),
+                    'Authorization' => 'Bearer ' . env('GITHUB_ACCESS_TOKEN'),
                     'X-GitHub-Api-Version' => '2022-11-28'
                 ]
             ]);
-            return $response->getBody();
+            $pullRequests = json_decode($response->getBody()->getContents(), true);
+            return $pullRequests;
         } catch (\Exception $e) {
             return response()->json(['error' => $e->getMessage()], 500);
         }
