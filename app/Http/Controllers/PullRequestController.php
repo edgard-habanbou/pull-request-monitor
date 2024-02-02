f936366e74e74e515da906b272658bffbb12ac1a
edgard <edgard.habanbou@hotmail.com>
2024-02-02 17:21:16 +0200

    Added error handling for the rate limit

--- app/Http/Controllers/PullRequestController.php
+++ app/Http/Controllers/PullRequestController.php
@@ -8,16 +8,37 @@ class PullRequestController extends Controller
 {
     public function fetchPullRequests($parameter)
     {
-        $client = new Client();
-        $response = $client->request('GET', 'https://api.github.com/search/issues?q=is:pr+is:open+repo:woocommerce/woocommerce' . $parameter, [
-            'headers' => [
-                'Accept' => 'application/vnd.github+json',
-                'Authorization' => 'Bearer ' . env('GITHUB_ACCESS_TOKEN'),
-                'X-GitHub-Api-Version' => '2022-11-28'
-            ]
-        ]);
-        $pullRequests = json_decode($response->getBody()->getContents(), true);
-        return $pullRequests["items"];
+        try {
+            // Fetch pull requests from GitHub
+            $client = new Client();
+            $response = $client->request('GET', 'https://api.github.com/search/issues?q=is:pr+is:open+repo:woocommerce/woocommerce' . $parameter, [
+                'headers' => [
+                    'Accept' => 'application/vnd.github+json',
+                    'Authorization' => 'Bearer ' . env('GITHUB_ACCESS_TOKEN'),
+                    'X-GitHub-Api-Version' => '2022-11-28'
+                ]
+            ]);
+
+            // Check if we're about to hit the rate limit
+            $remainingRequests = $response->getHeaderLine('X-RateLimit-Remaining');
+            if ($remainingRequests == 0) {
+                $resetTime = $response->getHeaderLine('X-RateLimit-Reset');
+                $resetTimeInSeconds = $resetTime - time();
+                sleep($resetTimeInSeconds); // Sleep until rate limit resets
+            }
+
+            // If the request is successful, return the pull requests
+            if ($response->getStatusCode() == 200) {
+                $pullRequests = json_decode($response->getBody()->getContents(), true);
+                return $pullRequests["items"];
+
+                // If the request fails, return an empty array
+            } else {
+                return [];
+            }
+        } catch (\GuzzleHttp\Exception\RequestException $e) {
+            return [];
+        }
     }
 
 
