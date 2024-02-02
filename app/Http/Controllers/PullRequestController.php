6a26cc13c55a54662d6de0275084f1a57913abf7
edgard <edgard.habanbou@hotmail.com>
2024-02-02 15:07:20 +0200

    Added PullRequestController functionality

--- app/Http/Controllers/PullRequestController.php
+++ app/Http/Controllers/PullRequestController.php
@@ -2,9 +2,24 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use GuzzleHttp\Client;
 
 class PullRequestController extends Controller
 {
-    //
+    public function fetchOpenPullRequests()
+    {
+        try {
+            $client = new Client();
+            $response = $client->request('GET', 'https://api.github.com/repos/OWNER/REPO/pulls', [
+                'headers' => [
+                    'Accept' => 'application/vnd.github+json',
+                    'Authorization' => 'Bearer ' + env('GITHUB_ACCESS_TOKEN'),
+                    'X-GitHub-Api-Version' => '2022-11-28'
+                ]
+            ]);
+            return $response->getBody();
+        } catch (\Exception $e) {
+            return response()->json(['error' => $e->getMessage()], 500);
+        }
+    }
 }
